903cf9e4914067305c138f4886554612c5c1c06c
Saitama267 <85219557+Saitama267@users.noreply.github.com>
2023-07-19 15:47:25 +0200

    Start API

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+  public class DataContext : DbContext
+  {
+    public DbSet<AppUser> Users { get; set; }
+    
+    public DataContext(DbContextOptions options) : base(options)
+    {
+      
+    }
+
+
+  }
+}
